89249adf41cfbf78a175fe5bed1665e0560d493e
malouk30 <malek.benjemia@esprit.tn>
2025-03-10 14:57:44 +0100

    ..

--- src/main/java/o/springback/repositories/GestionUserRepository/UserRepository.java
+++ src/main/java/o/springback/repositories/GestionUserRepository/UserRepository.java
@@ -3,6 +3,9 @@ import o.springback.entities.GestionUser.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByEmail(String username);
 }
