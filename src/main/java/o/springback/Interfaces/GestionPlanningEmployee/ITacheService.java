39fd58e096323b0754d663fc72d5070ce31c7525
BoughanmiMolka <157849165+BoughanmiMolka@users.noreply.github.com>
2025-04-02 19:16:40 +0100

    getNombreTachesParEmploye

--- src/main/java/o/springback/Interfaces/GestionPlanningEmployee/ITacheService.java
+++ src/main/java/o/springback/Interfaces/GestionPlanningEmployee/ITacheService.java
@@ -1,10 +1,13 @@
 package o.springback.Interfaces.GestionPlanningEmployee;
 import o.springback.entities.GestionPlanningEmployee.Tache;
 import java.util.List;
+import java.util.Map;
+
 public interface ITacheService {
     Tache save(Tache tache);
     Tache update(Tache tache);
     void delete(Long id);
     Tache findById(Long id);
     List<Tache> findAll();
+    Map<String, Object> getNombreTachesParEmploye(Long employeeId);
 }
--- src/main/java/o/springback/repositories/GestionPlanningEmployeeRepository/TacheRepository.java
+++ src/main/java/o/springback/repositories/GestionPlanningEmployeeRepository/TacheRepository.java
@@ -1,8 +1,15 @@
 package o.springback.repositories.GestionPlanningEmployeeRepository;
 import o.springback.entities.GestionPlanningEmployee.Tache;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TacheRepository extends JpaRepository<Tache, Long>{
+    //@Query("SELECT t.employee.idEmployee, COUNT(t) FROM Tache t WHERE t.employee IS NOT NULL GROUP BY t.employee.idEmployee")
+    @Query("SELECT t.employee.nom, t.employee.prenom, COUNT(t) FROM Tache t WHERE t.employee.idEmployee= :id GROUP BY t.employee.nom, t.employee.prenom")
+    List<Object[]> countTachesByEmployee(@Param("id") Long id);
 }
