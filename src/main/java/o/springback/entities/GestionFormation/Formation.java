fee7a6925da0c0f8a5c88126bd5c4f620ad450bf
Souid Ahmed <ahmedsouid008@gmail.com>
2025-04-15 21:09:52 +0100

    c

--- src/main/java/o/springback/entities/GestionFormation/Formation.java
+++ src/main/java/o/springback/entities/GestionFormation/Formation.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,26 +19,29 @@ public class Formation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idFormation;
-
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9\\s\\-']+$", message = "Le nom contient des caractères invalides")
     private String nom;
     private String description;
 
     @Temporal(TemporalType.DATE)
     @JsonFormat(pattern = "yyyy-MM-dd")
+    @FutureOrPresent
     private Date dateDebut;
 
     @Temporal(TemporalType.DATE)
     @JsonFormat(pattern = "yyyy-MM-dd")
-
+    @Future
     private Date dateFin;
 
     private String lieu;
     private boolean certification;
     @Column(name = "photoPath", length = 1000, nullable = true)
     private String photoPath;
-
+    @Min(10)
     private float noteMinPourCertificat;
-
+    @Min(value = 3)
+    @Max(value = 20)
     private int Capacity;
 
 
