bc13157c992c77ab583f97092a29d0280ba622f5
malouk30 <malek.benjemia@esprit.tn>
2025-04-16 10:36:00 +0100

    .

--- src/main/java/o/springback/SpringBackApplication.java
+++ src/main/java/o/springback/SpringBackApplication.java
@@ -1,9 +1,13 @@
 package o.springback;
 
+import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBackApplication {
 
     public static void main(String[] args) {
